141e4f7c29130327ed52d4576a9680cbc489457e
Pedro Braga <83465724+Pedro-H-Braga@users.noreply.github.com>
2022-11-25 20:51:52 -0300

    Update and rename script09_atualizado.py to script09.py

--- Lista 02/script09.py
+++ Lista 02/script09.py
@@ -16,28 +16,29 @@ a = True
 while a == True:
     print("INFORME A SEGUIR OS ANGULOS PARA VERIFICAR A QUE TIPO DE TRIANGULO SE ENCAIXA")
 
-    soma_180 = 180
+    contador_180 = 180
     
     ang1 = int(input("INFORME O ANGULO 1: "))      
-    bkp_180_ang1 = soma_180 - ang1 
+    bkp_180_ang1 = contador_180 - ang1 
     print(f"Falta ({bkp_180_ang1}°) para as somas do angulos serem --> 180° <--")
+    
     ang2 = int(input("INFORME O ANGULO 2: "))
     bkp_180_ang2 = bkp_180_ang1 - ang2
     print(f"Falta ({bkp_180_ang2}°) para as somas do angulos serem --> 180° <--")
+    
     ang3 = int(input("INFORME O ANGULO 3: "))
     bkp_180_ang3 = bkp_180_ang2 - ang3
     print(f"Falta ({bkp_180_ang3}°) para as somas do angulos serem --> 180° <--")
-    
-    #soma_angulos = ang1 + ang2 + ang3
-    # variavel de backup
-    # fazer um for para - quando pegar um valor mostrar quanto falta para atingir a soma de 180 - 
-
+        
     if ((((ang1 and ang2) and ang3) > 0) and ((ang1 + ang2 + ang3) == 180)) == True:
-        if ((ang1 and ang2 and ang3) < 90) == True:
+        
+        if (((ang1 < 90) and (ang2 < 90) and (ang3 < 90))) == True:
             print(f"({ang1};{ang2};{ang3}) Sao angulos de um --> Triangulo ACUTANGULO <--")
-        elif (((ang1 or ang2) or ang3) == 90) == True:
+    
+        elif (((ang1 == 90) or (ang2 == 90) or (ang3 == 90))) == True:
             print(f"({ang1};{ang2};{ang3}) Sao angulos de um --> Triangulo RETANGULO <--")
-        elif (((ang1 or ang2) or ang3) > 90) == True:
+    
+        elif (((ang1 > 90) or (ang2 > 90) or (ang3 > 90))) == True:
             print(f"({ang1}°;{ang2}°;{ang3}°) Sao angulos de um --> Triangulo OBTUSANGULO <--")
             
         else: print("-----VALOR INFORMADO NÃO CONDIZENTE COM O REQUERIDO-----")
